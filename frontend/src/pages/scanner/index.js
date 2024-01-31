b87e8c8cf2a87260d37f64cf1907d4e2e22522f8
Harsh Ghosalkar <ghosalkarharsh454@gmail.com>
2024-02-01 02:05:06 +0530

    updated scanner

--- frontend/src/pages/scanner/index.js
+++ frontend/src/pages/scanner/index.js
@@ -1,63 +1,114 @@
 // src/components/Camera.js
-import React, { useRef, useState } from 'react';
-// import './Camera.css'; // Import Tailwind CSS styles
+import React, { useRef, useCallback, useEffect, useState } from 'react';
+import Webcam from 'react-webcam';
+import { useRouter } from "next/router";
+
 
 const Camera = () => {
-  const videoRef = useRef(null);
-  const [cameraOpen, setCameraOpen] = useState(false);
+  const router = useRouter();
+  const webcamRef = useRef(null);
+  const [isScanning, setScanning] = useState(false);
+  const [capturedImage, setCapturedImage] = useState(null);
 
-  const openCamera = async () => {
-    try {
-      const stream = await navigator.mediaDevices.getUserMedia({ video: true });
-      if (videoRef.current) {
-        videoRef.current.srcObject = stream;
-        setCameraOpen(true);
+  const capture = useCallback(() => {
+    console.log("clicked1")
+    setScanning(true);
+    const imageSrc = webcamRef.current.getScreenshot();
+    setCapturedImage(imageSrc);
+
+    // Simulate a delay for scanning effect
+    setTimeout(() => {
+      setScanning(false);
+    }, 1000);
+
+    handleUpload()
+  }, []);
+
+
+//   const handleImageChange = (event) => {
+//     const file = event.target.files[0];
+//     setSelectedImage(file);
+//   };
+  
+  const handleUpload = () => {
+    
+    if (capturedImage) {
+        console.log("clicked")
+      const formData = new FormData();
+      formData.append('image', capturedImage);
+    //   setLoading(true);
+  
+      // Remove or update the Content-Type header
+      fetch('http://127.0.0.1:5000/packagedFood', {
+        method: 'POST',
+        body: formData,
+        mode: 'cors',
+        headers: {
+        }
+      })
+        .then(response => response.json())
+        .then(result => {
+          console.log(result)
+          const newItems = result
+        })
+        .finally(() => {
+        //   setLoading(false);
+        })
+  
+        .catch(error => console.log('error', error));
+    } else {
+      console.warn('No image selected');
+    }
+  };
+
+
+  useEffect(() => {
+    const openCamera = async () => {
+      try {
+        await navigator.mediaDevices.getUserMedia({ video: true });
+      } catch (error) {
+        console.error('Error accessing camera:', error);
       }
-    } catch (error) {
-      console.error('Error accessing camera:', error);
-    }
-  };
+    };
 
-  const closeCamera = () => {
-    if (videoRef.current && videoRef.current.srcObject) {
-      const stream = videoRef.current.srcObject;
-      const tracks = stream.getTracks();
-      tracks.forEach((track) => track.stop());
-
-      videoRef.current.srcObject = null;
-      setCameraOpen(false);
-    }
-  };
+    openCamera();
+  }, []);
 
   return (
-    <div className="flex items-center justify-center h-screen bg-gray-200">
-      <div className={`relative w-full h-full ${cameraOpen ? '' : 'border-4 border-green-500'}`}>
-        {cameraOpen ? (
-          <video
-            ref={videoRef}
-            autoPlay
-            playsInline // Add playsInline for iOS support
-            className="w-full h-full"
-          />
-        ) : (
-          <div className="absolute inset-0 bg-black"></div>
-        )}
-        {cameraOpen && (
-          <button
-            onClick={closeCamera}
-            className="absolute top-4 right-4 bg-red-500 text-white px-4 py-2 rounded-md focus:outline-none"
-          >
-            Close Camera
-          </button>
-        )}
-      </div>
-      {!cameraOpen && (
+    <div className="flex flex-col items-center justify-center h-screen bg-gray-200">
+      <h1 className=" mb-[15px] text-4xl font-md mt-4">Scan Food</h1>
+      <div className="flex flex-col  items-center relative w-[80%] h-4/5 rounded-[17px] overflow-hidden">
+        <Webcam
+          audio={false}
+          ref={webcamRef}
+          screenshotFormat="image/jpeg"
+          className={`w-full rounded-[17px]  overflow-hidden  ${isScanning ? 'animate-scanning' : ''}`}
+        />
         <button
-          onClick={openCamera}
-          className="bg-green-500 text-white px-4 py-2 rounded-md mt-4 focus:outline-none"
+        onClick={capture}
+        className=" mt-[25px] bg-blue-500 text-white px-6 py-2 rounded-md focus:outline-none"
+      >
+        Capture Image
+      </button>
+
+      <div className="flex items-center my-2 space-x-2">
+          <span className="text-gray-500">OR</span>
+          <div className="border-t border-gray-500 flex-1"></div>
+        </div>
+        <button
+          onClick={() => router.push("/packaged")}
+          className="bg-green-500 text-white px-6 py-2 rounded-md focus:outline-none"
         >
-          Open Camera
+          Upload Food Image
         </button>
+
+      </div>
+      
+      {capturedImage && (
+        <div className="mt-4">
+          <h2 className="text-xl font-semibold mb-2">Captured Image</h2>
+          <img src={capturedImage} alt="Captured" className="max-w-md w-full" />
+        </div>
       )}
     </div>
   );
