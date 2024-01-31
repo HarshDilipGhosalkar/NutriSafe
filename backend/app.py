7846382f103062f431d522eae0c0f3976d7128b8
Rupesh Raut <rupeshraut99396@gmail.com>
2024-02-01 00:13:46 +0530

    made alternate foods APIs

--- backend/app.py
+++ backend/app.py
@@ -8,6 +8,8 @@ from resources.scanner import (PackagedFood, Food)
 from resources.recipe import (Recipe, RecipeFromFoodItemsAtHome)
 from resources.message import Message
 from resources.report import Report
+# from backend.resources.alternateFoodsOld import (AlternateAllergicFoods, AlternateNonAllergicFoods)
+from resources.alternateFoods import (AlternateAllergicFoods, AlternateNonAllergicFoods)
 from mongo_engine import db
 from flask_jwt_extended import JWTManager
 from flask_cors import CORS
@@ -36,6 +38,8 @@ api.add_resource(Recipe, "/recipe")
 api.add_resource(RecipeFromFoodItemsAtHome, '/recipeFromFoodItemsAtHome')
 api.add_resource(Message, "/message")
 api.add_resource(Report, "/report")
+api.add_resource(AlternateAllergicFoods, "/alternateAllergicFoods")
+api.add_resource(AlternateNonAllergicFoods, "/alternateNonAllergicFoods")
 
 if __name__ == "__main__":
     app.run(debug=True)
