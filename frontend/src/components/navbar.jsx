f75455b392c33cbe7418068689c6f6f45e406580
Jhenil Parihar <jhenilparihar@gmail.com>
2024-02-01 01:32:27 +0530

    commit

--- frontend/src/components/navbar.jsx
+++ frontend/src/components/navbar.jsx
@@ -4,12 +4,12 @@ const Navbar = () => {
   return (
     <div className="flex border-[1px] justify-between h-[60px] py-[10px] px-[15px] mb-4">
       <div className="flex gap-x-[10px] items-center">
-        <img className="h-[40px] w-[40px]" src="assets/logo.png"></img>
+        <img className="h-[40px] w-[40px]" src="/assets/logo.png"></img>
         <p className="text-[20px] flex w-full font-bold">Nutrisafe</p>
       </div>
       <img
         className="h-[40px] w-[40px] rounded-full"
-        src="assets/profile.png"
+        src="/assets/profile.png"
       ></img>
     </div>
   );
