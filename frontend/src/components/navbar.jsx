b1275a543e99671826557fdd7ea8ea6cbeaec11c
Jhenil Parihar <jhenilparihar@gmail.com>
2024-01-31 20:10:24 +0530

    layout

---
+++ frontend/src/components/navbar.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="flex border-[1px] justify-between py-[10px] px-[15px] mb-4">
+      <div className="flex gap-x-[10px] items-center">
+        <img className="h-[40px] w-[40px]" src="assets/logo.png"></img>
+        <p className="text-[20px] flex w-full font-bold">Nutrisafe</p>
+      </div>
+      <img
+        className="h-[40px] w-[40px] rounded-full"
+        src="assets/profile.png"
+      ></img>
+    </div>
+  );
+};
+
+export default Navbar;
--- frontend/src/pages/_app.js
+++ frontend/src/pages/_app.js
@@ -1,5 +1,19 @@
-import '@/styles/globals.css'
+import Layout from "@/components/Layout";
+import { BottomBar } from "@/components/bottomBar";
+import Navbar from "@/components/navbar";
+import "@/styles/globals.css";
+// import AppContextProvider from "@/context/appContext";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    // <AppContextProvider>
+    // <Layout>
+    <>
+      <Navbar />
+      <Component {...pageProps} />
+      <BottomBar />
+    </>
+    // </Layout>
+    // </AppContextProvider>
+  );
 }
