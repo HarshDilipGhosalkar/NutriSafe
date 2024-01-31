7ed8ee0d5a9e0e1363e969a13d208652a6fc7ad0
Rupesh Raut <rupeshraut99396@gmail.com>
2024-02-01 02:12:45 +0530

    deployment fixes

--- backend/app.py
+++ backend/app.py
@@ -8,10 +8,8 @@ from resources.scanner import (PackagedFood, Food)
 from resources.recipe import (Recipe, RecipeFromFoodItemsAtHome)
 from resources.message import Message
 from resources.report import Report
-# from backend.resources.alternateFoodsOld import (AlternateAllergicFoods, AlternateNonAllergicFoods)
 from resources.alternateFoods import (AlternateAllergicFoods, AlternateNonAllergicFoods)
 from mongo_engine import db
-from flask_jwt_extended import JWTManager
 from flask_cors import CORS
 import os
 
@@ -19,9 +17,6 @@ app = Flask(__name__)
 api = Api(app)
 CORS(app)
 
-app.config["JWT_SECRET_KEY"] = "all_stackers_going_to_win_hackathon"
-
-jwt = JWTManager(app)
 
 DB_URI = os.getenv("FLASK_MONGODB_URI")
 
