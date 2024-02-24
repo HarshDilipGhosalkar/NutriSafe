47488fb25988f1caae8a02d3f1ca2f2d50285305
Harsh Ghosalkar <ghosalkarharsh454@gmail.com>
2024-02-24 15:08:33 +0530

    updates

--- backend/resources/prompt.py
+++ backend/resources/prompt.py
@@ -1,4 +1,4 @@
-prompt1 = """I will tell you the plant name and some symptoms of disease on that plant. You should analyse them and tell me what disease the crop may be having and the treatment for that disease and also provide specific fertilizers. 
+prompt1 = """I will tell you the plant/crop name and disease it is suffering through and also description of the crop/plant. You should analyse them and tell the treatment for that disease and also provide specific fertilizers. 
     Format the symptoms as follows. Output should be strictly in json format and should strictly contain nothing extra before and after that:
     {"DiseaseName": "XYZ",
     "Treatment": [
